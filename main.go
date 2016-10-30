86c3cfe9e63cc1d3a0a7ec64cc6cf7bba071f46d
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2016-10-30 14:08:06 +0100

    Various

--- main.go
+++ main.go
@@ -38,75 +38,74 @@ func main() {
 	}
 
 	tmplStr := `
+<style type="text/css">
+	body {
+	    font-family: Helvetica Neue;
+	    font-size: 16px;
+	}
+
+	.group-title {
+	    clear: both;
+	    margin-top: 30px;
+	}
+
+	.message {
+	    padding: 5px;
+	    border: 1px solid #d6d6d6;
+	    margin-top: 10px;
+	    border-radius: 5px;
+	}
+
+	.message-body {
+	    margin:0;
+	}
+
+	.message-time {
+	    display: block;
+	    font-size: 0.8em;
+	    padding-top: 5px;
+	}
+
+	.message-sent {
+	    float:right;
+	    background-color: #e8e8e8;
+	}
+
+	.message-sent .message-time {
+	    color: #8c8c8c;
+	}
+
+	.message-received {
+	    float:left;
+	    background-color: #4e97ff;
+	    color: white;
+	}
+
+	.message-received .message-time {
+	    color: #d8d8d8;
+	}
+
+	.message {
+	    clear:both;
+	}
+
+	.message-sender {
+	    font-size: 0.9em;
+	    margin: 0;
+	    margin-bottom: 5px;
+	}
+
+	.message-sent .message-sender {
+			display: none;
+	}
+
+	.message-attachments img, .message-attachments video {
+		max-width: 200px;
+		display: block;
+	}
+</style>
+
 {{range .}}
-	<style type="text/css">
-		body {
-		    font-family: Helvetica Neue;
-		    font-size: 16px;
-		}
-
-		.group-title {
-		    clear: both;
-		    margin-top: 30px;
-		}
-
-		.message {
-		    padding: 5px;
-		    border: 1px solid #d6d6d6;
-		    margin-top: 10px;
-		    border-radius: 5px;
-		}
-
-		.message-body {
-		    margin:0;
-		}
-
-		.message-time {
-		    display: block;
-		    font-size: 0.8em;
-		    padding-top: 5px;
-		}
-
-		.message-sent {
-		    float:right;
-		    background-color: #e8e8e8;
-		}
-
-		.message-sent .message-time {
-		    color: #8c8c8c;
-		}
-
-		.message-received {
-		    float:left;
-		    background-color: #4e97ff;
-		    color: white;
-		}
-
-		.message-received .message-time {
-		    color: #d8d8d8;
-		}
-
-		.message {
-		    clear:both;
-		}
-
-		.message-sender {
-		    font-size: 0.9em;
-		    margin: 0;
-		    margin-bottom: 5px;
-		}
-
-		.message-sent .message-sender {
-				display: none;
-		}
-
-		.message-attachments img, .message-attachments video {
-			max-width: 200px;
-			display: block;
-		}
-	}
-	}
-	</style>
 	<h1 class="group-title">{{.Group}}</h1>
 
 	{{range .Messages}}
@@ -115,7 +114,7 @@ func main() {
 				{{.Sender}}
 			</h2>
 			<p class="message-body">
-				{{.Content}}
+				{{.Content | formatContent }}
 			</p>
 			<div class="message-attachments">
 				{{ range .Attachments }}
@@ -123,6 +122,8 @@ func main() {
 						<img src="/serve?path={{ .URL }}" />
 					{{ else if eq .Kind "video" }}
 						<video controls src="/serve?path={{.URL}}" />
+					{{ else if eq .Kind "vcard" }}
+						<a href="/serve?path={{.URL}}">download vcard</a>
 					{{ end }}
 				{{ end }}
 			</div>
@@ -131,10 +132,11 @@ func main() {
 			</small>
 		</div>
 	{{ end }}
+
 {{ end }}
 `
 
-	tmpl, err = template.New("msgs").Parse(tmplStr)
+	tmpl, err = template.New("msgs").Funcs(template.FuncMap{"hasPrefix": hasPrefix, "toURL": toURL, "formatContent": formatContent}).Parse(tmplStr)
 	if err != nil {
 		panic(err)
 	}
@@ -174,12 +176,28 @@ func serveHandler(w http.ResponseWriter, r *http.Request) {
 
 	file, err := os.Open(path)
 	if err != nil {
-		panic(err)
+		log.Printf("ERROR: %s", err)
+		w.WriteHeader(500)
 	}
 
 	_, err = io.Copy(w, file)
 	if err != nil {
-		panic(err)
+		log.Printf("ERROR: %s", err)
+		w.WriteHeader(500)
 	}
 
+}
+
+func hasPrefix(s, prefix string) bool {
+	return strings.HasPrefix(s, prefix)
+}
+
+func toURL(s string) template.URL {
+	return template.URL(s)
+}
+
+func formatContent(s string) template.HTML {
+	s = strings.Replace(s, "\n", "<br>", -1)
+	s = strings.Replace(s, "\r", "<br>", -1)
+	return template.HTML(s)
 }
