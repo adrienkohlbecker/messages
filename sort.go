81233128b6a756ea7cce8a0cadc21e7ffbba0d45
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2017-01-07 13:24:01 +0100

    Fix

--- sort.go
+++ sort.go
@@ -1,24 +1,24 @@
 package main
 
-// import (
-// 	"log"
-// 	"sort"
-//
-// 	"github.com/adrienkohlbecker/messages/model"
-// )
-//
-// func main() {
-//
-// 	msgs, err := model.Load("/Users/adrien/Desktop/messages-store/signal_g4.json")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-//
-// 	sort.Sort(msgs)
-//
-// 	err = msgs.Write("/Users/adrien/Desktop/messages-store/signal_g4.json")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-//
-// }
+import (
+	"log"
+	"sort"
+
+	"github.com/adrienkohlbecker/messages/model"
+)
+
+func main() {
+
+	msgs, err := model.Load("/Users/adrien/Desktop/messages-store/whatsapp.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sort.Sort(msgs)
+
+	err = msgs.Write("/Users/adrien/Desktop/messages-store/whatsapp.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
